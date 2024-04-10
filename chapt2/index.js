aecf6cfac0adeb2eebfcb784abe7f693fc64a64b
Eric Ross <epr@rossmbox.com>
2024-04-09 19:49:50 -0700

    Add template

--- chapt2/index.js
+++ chapt2/index.js
@@ -1,8 +1,11 @@
 const express = require('express');
 const app = express();
+const path = require('path');
+const rootDir = require('./util/path');
 
+app.set('view engine', 'ejs');
 app.use('/',(req, res, next) => {
-    res.send("<p>Hello</p>");
+    res.render(path.join(rootDir, 'public', 'index'), {"fun": "It is fun"});
 });
 
 app.listen(3000);
